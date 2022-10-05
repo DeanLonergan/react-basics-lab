cfe8aa6c256dcd2ca010d75e46ffccb094bd2d58
Dean Lonergan <74914758+DeanLonergan@users.noreply.github.com>
2022-10-05 13:03:04 +0100

    Exercise 1 completed.

--- src/components/exercises/01_staticComponent.js
+++ src/components/exercises/01_staticComponent.js
@@ -3,27 +3,50 @@ import "../../../node_modules/bootstrap/dist/css/bootstrap.css";
 
 const Demo = () => {
   return (
-    <table className="table table-bordered">
-    <thead>
-      <tr>
-        <th>Name</th>
-        <th>No lectures</th>
-        <th>No practicals</th>
-      </tr>
-    </thead>
-    <tbody >
-      <tr>
-        <td>DevOps</td>
-        <td>2</td>
-        <td>2</td>
-      </tr>
-      <tr>
-        <td>Enterprise Web Dev</td>
-        <td>3</td>
-        <td>2</td>
-      </tr>
-    </tbody >
-  </table>
+    <div>
+      <h2>Applied Computing - Semester 5</h2>
+      <table className="table table-bordered">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>No lectures</th>
+          <th>No practicals</th>
+        </tr>
+      </thead>
+      <tbody >
+        <tr>
+          <td>Developer Operations</td>
+          <td>2</td>
+          <td>2</td>
+        </tr>
+        <tr>
+          <td>Web App Dev 2</td>
+          <td>3</td>
+          <td>2</td>
+        </tr>
+        <tr>
+          <td>NoSQL Databases</td>
+          <td>2</td>
+          <td>3</td>
+        </tr>
+        <tr>
+          <td>Model Based Development</td>
+          <td>1</td>
+          <td>4</td>
+        </tr>
+        <tr>
+          <td>Professional Practice</td>
+          <td>3</td>
+          <td>2</td>
+        </tr>
+        <tr>
+          <td>Measurement Systems</td>
+          <td>3</td>
+          <td>2</td>
+        </tr>
+      </tbody >
+    </table>
+  </div>
   );
 };
 
